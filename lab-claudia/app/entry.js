08ecfe1e67bf547a9479bceb53116a288f9ccec6
Claudia <claudia.cedfeldt@gmail.com>
2016-10-18 13:39:30 -0700

    cowz 4 lyfe

--- lab-claudia/app/entry.js
+++ lab-claudia/app/entry.js
@@ -10,29 +10,71 @@ const angular = require('angular');
 // app modules
 
 // angular module
+// creates an angular module called demoA (has 2 arguments - setter)
 const demoApp = angular.module('demoApp', []);
+//demoApp is the entry point to the app
+//ng-app is demo app- route module is bound to this tag
+//angular can only be done inside this tag
 
-// angular constructus
-demoApp.controller('CowsayController', [ '$log', '$scope', CowsayController]);
+// angular constructor - all controllers are constructors
+// services can be injected into controller
+// services are helper functions - lib directory - supporting code
+demoApp.controller('CowsayController', [ '$log', CowsayController]);
 
-function CowsayController($log, $scope){
-  $log.debug('init CowsayController');
-  let cowsayCtrl = $scope.cowsayCtrl = {};
-  cowsayCtrl.title = 'Derp!';
-  cowsayCtrl.data = [];
+function CowsayController($log){
+  $log.debug('init cowsayCtrl');
+  this.title = 'Derp!';
+  this.data = [];
 
-  cowsayCtrl.updateCow = function(input){
-    $log.debug('cowsayCtrl.updateCow()');
+  cowsay.list((err, cowfiles) => {
+    this.cowfiles = cowfiles;
+    this.currentCow = this.cowfiles[0];
+    console.log('this.cowfiles', this.cowfiles);
+  });
+
+  this.updateCow = function(input){
+    $log.debug('this.updateCow()');
     return '\n' + cowsay.say({text: input || 'give me all the says'});
   };
-  cowsayCtrl.helloClick = function(input){
-    $log.debug('cowsayCtrl.helloClick()');
-    cowsayCtrl.data.push(input);
-    cowsayCtrl.populate = cowsayCtrl.updateCow(input);
+  this.helloClick = function(input){
+    $log.debug('this.helloClick()');
+    this.data.push(input);
+    this.populate = this.updateCow(input);
 
   };
-  cowsayCtrl.resetClick = function(){
-    $log.debug('cowsayCtrl.resetClick()');
-    cowsayCtrl.populate =cowsayCtrl.updateCow(cowsayCtrl.data.pop());
+  this.resetClick = function(){
+    $log.debug('this.resetClick()');
+    this.populate = this.updateCow(this.data.pop() || '');
   };
+}
+
+//modern form of controller
+demoApp.controller('NavController', [ '$log', NavController]);
+
+//navcontroller as controller - same as using the $scope
+//navctrl is created on the scope - created in the html
+//if there is no 'as + __ctrl', it will expect the scope to be injected and not work
+//allows us to use 'this' - less things to inject
+
+function NavController($log){
+  $log.debug('init NavCtrl');
+  //makes the object thing on its own and everything is added to that scope
+  this.routes = [
+    {
+      name: 'Home',
+      url: '/home',
+    },
+    {
+      name: 'About',
+      url: '/about',
+    },
+    {
+      name: 'Cow Facts',
+      url: '/cowfacts',
+    },
+    {
+      name: 'Contact',
+      url: '/contact',
+    },
+  ];
 }
--- lab-claudia/webpack.config.js
+++ lab-claudia/webpack.config.js
@@ -1,24 +1,44 @@
 'use strict';
 
 const HTMLPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: `${__dirname}/app/entry.js`,
+  entry: `${__dirname}/app/entry.js`, //entry points of the program
   output: {
-    filename: 'bundle.js',
-    path: 'build',
+    filename: 'bundle.js', //name of the bundle
+    path: 'build', //
   },
+  // wrote our own template so there isn't default behavior
   plugins: [
     new HTMLPlugin({
       template: `${__dirname}/app/index.html`,
     }),
+    // takes output of css loader and turn it into bundle css
+    new ExtractTextPlugin('bundle.css'),
   ],
   module: {
     loaders: [
       {
-        test: /\.scss$/,
-        loader: 'style!css!sass!',
+        test: /\.js$/, //$ - regex that means end of line
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+      {
+        test: /\.scss$/, //regex to match file exression - run loader on all files that match this expression
+        loader: ExtractTextPlugin.extract('style', 'css!resolve-url!sass?sourceMap'), //match loaders chain - goes right to left
+        //style loader- injects style into page
+        //style loader is backup
+        //resolve url uses relative paths- used for font awesome which uses relative paths
+      },
+      {
+        test:/\.(eot|woff|ttf|svg).*/, //.* -> any other characters
+        loader: 'url?limit=10000&name=fonts/[hash].[ext]', //10000 bytes
+        // if less than 10000, put in, else put it in fonts directory
+
       },
     ],
   },
 };
+
+//killall node turns off webpack dev-server
